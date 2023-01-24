16204d362d51424609708af3549baa16d2e73d52
Giveen <ajaxx20020@gmail.com>
2023-01-24 13:58:17 -0700

    Update default.js

--- builds/default.js
+++ builds/default.js
@@ -1,5 +1,5 @@
 module.exports = {
-    board_env: "BOARD_CREALITY_V427",
+    board_env: " STM32F103RE_creality",
     meta: {
         stable_name: "ender_3_4.2.7-{{marlin_version}}-default-{{uid}}",
         nightly_name: "ender_3_4.2.7-{{current_date}}-default-{{uid}}"
