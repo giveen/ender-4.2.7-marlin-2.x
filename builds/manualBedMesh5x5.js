1126efbef1186a2eab955a1658376862747ebe95
Giveen <ajaxx20020@gmail.com>
2023-01-24 12:53:59 -0700

    Create manualBedMesh5x5.js

---
+++ builds/manualBedMesh5x5.js
@@ -0,0 +1,14 @@
+// builds/manualBedMesh5x5.js
+module.exports = {
+    extends: "builds/base.js", //we can extend and include at the same time
+    include: "builds/manualBedMesh.js", // <--
+    meta: {
+        stable_name: "ender_3_4.2.2-{{marlin_version}}-manual_mesh_5x5-{{uid}}",
+        nightly_name: "ender_3_4.2.2-{{current_date}}-manual_mesh_5x5-{{uid}}"
+    },
+    configuration: {
+        enable: [
+            ["GRID_MAX_POINTS_X", 5] // only the grid size is defined here
+        ]
+    }
+};
