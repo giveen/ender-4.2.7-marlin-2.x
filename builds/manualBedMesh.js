4db9e2375ea8d239334bed0d187756032103d663
Giveen <ajaxx20020@gmail.com>
2023-01-24 20:15:33 -0700

    Update manualBedMesh.js

--- builds/manualBedMesh.js
+++ builds/manualBedMesh.js
@@ -9,8 +9,6 @@ module.exports = {
             "RESTORE_LEVELING_AFTER_G28",
             "LCD_BED_LEVELING",
             "MESH_EDIT_MENU"
-            ["HOMING_FEEDRATE_MM_M" [20*60,20*60,8*60]
         ]
-        disable: ["ARC_SUPPORT"]
     }
 };
