25f043606d27a46cec4dac877bac002d0806fa0f
Giveen <ajaxx20020@gmail.com>
2023-01-27 10:14:08 -0700

    Update manualBedMesh.js

--- builds/manualBedMesh.js
+++ builds/manualBedMesh.js
@@ -1,4 +1,3 @@
-// builds/manualBedMesh.js
 module.exports = {
     partial: true, // <--
     configuration: {
@@ -9,6 +8,21 @@ module.exports = {
             "RESTORE_LEVELING_AFTER_G28",
             "LCD_BED_LEVELING",
             "MESH_EDIT_MENU"
+            "BED_TRAMMING_INCLUDE_CENTER",
+            "S_CURVE_ACCELERATION",
+            "INDIVIDUAL_AXIS_HOMING_MENU",
+            ["DEFAULT_MAX_FEEDRATE", [500, 500, 20, 5]]
+            "PREHEAT_BEFORE_LEVELING",
+            "LEVELING_NOZZLE_TEMP, 190",
+            "LEVELING_BED_TEMP 55"
+        ]
+    }
+    configuration_adv: {
+        enable: [
+            "EMERGENCY_PARSER",
+            "ADVANCED_PAUSE_FEATURE",
+            "EMERGENCY_PARSER",
+            "ADVANCED_PAUSE_FEATURE"
         ]
     }
 };
