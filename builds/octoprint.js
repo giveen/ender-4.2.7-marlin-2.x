87ef654e70120fb8abbc7318641cceec7b624100
Giveen <ajaxx20020@gmail.com>
2023-01-29 06:06:31 -0700

    Update octoprint.js

--- builds/octoprint.js
+++ builds/octoprint.js
@@ -1,7 +1,7 @@
 // builds/manualBedMesh5x5.js
 module.exports = {
     extends: "builds/base.js", //we can extend and include at the same time
-    include: "builds/manualBedMesh.js", // <--
+    include: "builds/manualBedMesh5x5.js",// <--
     meta: {
         stable_name: "ender_3_4.2.7-{{marlin_version}}-octo-manual_mesh_5x5-{{uid}}",
         nightly_name: "ender_3_4.2.7-{{current_date}}-octo-manual_mesh_5x5-{{uid}}"
@@ -9,8 +9,21 @@ module.exports = {
     configuration: {
         enable: [
             ["GRID_MAX_POINTS_X", 5], // only the grid size is defined here
-            "SERIAL_PORT_3 1",
-            ["BAUDRATE_3", 250000]            
+         ],
+    },
+    configuration_adv: {
+        enable: [
+            "SOUND_MENU_ITEM",
+            "HOST_ACTION_COMMANDS",
+            "AUTO_REPORT_TEMPERATURES",
+            "AUTO_REPORT_POSITION",
+            "M114_DETAIL",
+            "M114_REALTIME",
+            "EXTENDED_CAPABILITIES_REPORT",
+            "REPORT_FAN_CHANGE",
+            "GCODE_CASE_INSENSITIVE",
+            "HOST_PROMPT_SUPPORT",
+            "HOST_START_MENU_ITEM"
         ]
     }
 };
