fbb2d8821d68caed86b3f06d9c09012d93f4e7df
Giveen <ajaxx20020@gmail.com>
2023-01-28 20:11:46 -0700

    Update octoprint.js

--- builds/octoprint.js
+++ builds/octoprint.js
@@ -3,8 +3,8 @@ module.exports = {
     extends: "builds/base.js", //we can extend and include at the same time
     include: "builds/manualBedMesh.js", // <--
     meta: {
-        stable_name: "ender_3_4.2.7-{{marlin_version}}-manual_mesh_5x5-{{uid}}",
-        nightly_name: "ender_3_4.2.7-{{current_date}}-manual_mesh_5x5-{{uid}}"
+        stable_name: "ender_3_4.2.7-{{marlin_version}}-octo-manual_mesh_5x5-{{uid}}",
+        nightly_name: "ender_3_4.2.7-{{current_date}}-octo-manual_mesh_5x5-{{uid}}"
     },
     configuration: {
         enable: [
