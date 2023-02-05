1aaf469fc01df3708cadc304b54cda6d84178dc1
Giveen <ajaxx20020@gmail.com>
2023-02-05 06:25:59 -0700

    Update autolevel.js

--- builds/autolevel.js
+++ builds/autolevel.js
@@ -1,10 +1,5 @@
 module.exports = {
-    extends: "builds/base.js",
     partial: true,
-    meta: {
-        stable_name: "ender_3_4.2.7-{{marlin_version}}-auto_level_5x5-{{uid}}",
-        nightly_name: "ender_3_4.2.7-{{current_date}}-auto_level_5x5-{{uid}}"
-    },
     configuration: {
         enable: [
             "BLTOUCH",
