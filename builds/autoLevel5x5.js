7c66e72c04a209df0c2ea746f2b25e1ae057b2a5
Giveen <ajaxx20020@gmail.com>
2023-02-05 05:51:37 -0700

    Update autoLevel5x5.js

--- builds/autoLevel5x5.js
+++ builds/autoLevel5x5.js
@@ -1,7 +1,6 @@
 module.exports = {
-    extends: "builds/base/base.js",
-    extends: "builds/features/autoLevel.js",
-    partial: true,
+    extends: "builds/base.js",
+    include: "builds/features/autoLevel.js",
     meta: {
         stable_name: "ender_3_4.2.2-{{marlin_version}}-auto_level_5x5-{{uid}}",
         nightly_name: "ender_3_4.2.2-{{current_date}}-auto_level_5x5-{{uid}}"
