05e05704c041067c351c79a3f86da54b1e74f1e0
Giveen <ajaxx20020@gmail.com>
2023-02-05 05:27:58 -0700

    Update manualBedMesh5x5.js

--- builds/manualBedMesh5x5.js
+++ builds/manualBedMesh5x5.js
@@ -2,7 +2,7 @@
 module.exports = {
     partial: true,
     extends: "builds/base/base.js", //we can extend and include at the same time
-    include: "builds/features/manualBedMesh.js", // <--
+    extends: "builds/features/manualBedMesh.js", // <--
     meta: {
         stable_name: "ender_3_4.2.7-{{marlin_version}}-manual_mesh_5x5-{{uid}}",
         nightly_name: "ender_3_4.2.7-{{current_date}}-manual_mesh_5x5-{{uid}}"
