96769ac7116c9359fe5ee6751d44b567a3ba1d3b
Giveen <ajaxx20020@gmail.com>
2023-02-05 16:56:15 -0700

    Update autoLevel.js

--- builds/autoLevel.js
+++ builds/autoLevel.js
@@ -7,12 +7,9 @@ module.exports = {
             "RESTORE_LEVELING_AFTER_G28",
             "Z_SAFE_HOMING",
             "USE_PROBE_FOR_Z_HOMING",
-            ["Z_MIN_PROBE_PIN", q`PB1`],
             "LCD_BED_LEVELING",
-
             ["DEFAULT_LEVELING_FADE_HEIGHT", 20],
-            ["NOZZLE_TO_PROBE_OFFSET", [0, 0, 0]],
-
+            ["NOZZLE_TO_PROBE_OFFSET", [-49, -9, 0]],
             //M48 test
             "Z_MIN_PROBE_REPEATABILITY_TEST"
         ],
@@ -25,7 +22,6 @@ module.exports = {
             "BABYSTEPPING",
             "BABYSTEP_ZPROBE_OFFSET",
             "PROBE_OFFSET_WIZARD",
-
             //symmetrical
             ["PROBING_MARGIN_LEFT", 45],
             ["PROBING_MARGIN_RIGHT", 45],
