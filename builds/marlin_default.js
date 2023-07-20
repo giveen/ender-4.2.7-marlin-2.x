aaa5c5a8614182ad6beead644364baccf0e079fb
Giveen <ajaxx20020@gmail.com>
2023-07-20 06:28:26 -0600

    Update marlin_default.js

--- builds/marlin_default.js
+++ builds/marlin_default.js
@@ -1,7 +1,7 @@
 module.exports = {
     board_env: "STM32F103RE_creality",
     meta: {
-        stable_name: "ender_3_4.2.7-{{marlin_version}}-default-{{uid}}",
+        stable_name: "ender_3_4.2.7-{{marlin_version}}-default-{{uid}}"
     },
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
