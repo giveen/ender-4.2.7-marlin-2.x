99061492a4fa64475607ba3558e02e738e41b9ed
Giveen <ajaxx20020@gmail.com>
2023-07-27 13:56:15 -0600

    Update autoLevel5x5.js

--- builds/autoLevel5x5.js
+++ builds/autoLevel5x5.js
@@ -2,7 +2,7 @@ module.exports = {
     extends: "builds/base.js",
     include: "builds/autoLevel.js",
     meta: {
-        stable_name: "ender_3_4.2.7-{{marlin_version}}-auto_level_5x5-{{uid}}"
+        stable_name: "ender_3_4.2.2-{{marlin_version}}-auto_level_5x5-{{uid}}"
     },
     configuration: {
         enable: [
