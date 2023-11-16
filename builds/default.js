3b58d0c7f6109cc3fa32e68e67241e716f37d34c
Giveen <ajaxx20020@gmail.com>
2023-11-16 16:32:51 -0700

    Update and rename marlin_default.js to default.js

--- builds/default.js
+++ builds/default.js
@@ -2,11 +2,13 @@ module.exports = {
     board_env: "STM32F103RE_creality",
     min_version: "2.1.2",
     meta: {
-        stable_name: "ender3_4.2.7-{{marlin_version}}-default-{{uid}}"
+        stable_name: "ender_3_4.2.7-{{marlin_version}}-default-{{uid}}",
+        nightly_name: "ender_3_4.2.7-{{current_date}}-default-{{uid}}"
     },
     based_on: {
         repo: "https://github.com/MarlinFirmware/Configurations/",
         path: "/config/examples/Creality/Ender-3/CrealityV427/",
-        stable_branch: "release-{{marlin_version}}"
+        stable_branch: "release-{{marlin_version}}",
+        nightly_branch: "bugfix-2.1.x"
     }
 };
